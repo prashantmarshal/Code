be1160ed09d175c567ecbf60ccaf6b2265b86836
Prashant <prashanthp@iitrpr.ac.in>
2017-12-07 00:22:57 +0530

    data section
    Signed-off-by: Prashant <prashanthp@iitrpr.ac.in>

--- codetheory/datasection.c
+++ codetheory/datasection.c
@@ -4,6 +4,14 @@ int avar[1000] = {1}; // data
 int bvar; // bss
 static int cvar; // bss
 static int dvar = 2; // data
+static int dvar = 2; // data
+static int globalzerostatic = 0; // bss
+
+void f(){
+       int notstatic;
+       static int localstatic = 5; // data
+       static int zerostatic = 0; // bss
+}
 
 int main(){
 	static int evar = 2; // data
