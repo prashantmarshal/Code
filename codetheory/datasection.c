2589d2b02b11a3793ca82147f08d11c57fc11c80
Prashant <prashanthp@iitrpr.ac.in>
2017-12-07 03:15:35 +0530

    data section

--- codetheory/datasection.c
+++ codetheory/datasection.c
@@ -4,16 +4,22 @@ int avar[1000] = {1}; // data
 int bvar; // bss
 static int cvar; // bss
 static int dvar = 2; // data
-static int dvar = 2; // data
 static int globalzerostatic = 0; // bss
 
 void f(){
-       int notstatic;
-       static int localstatic = 5; // data
-       static int zerostatic = 0; // bss
+	int notstatic;
+	static int localstatic = 5; // data
+	static int zerostatic = 0; // bss
 }
 
+const char* conststring = "prashant"; // data
+char *nonconststring = "patil"; // data
+static const char* staticconststring = "prashant"; // data
+static char *staticnonconststring = "patil"; // data
+
 int main(){
+	static const char* localstaticconststring = "prashant"; // data
+	static char *localstaticnonconststring = "patil"; // data
 	static int evar = 2; // data
 	static int fvar; // bss
 }
