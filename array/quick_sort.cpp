65d2d52fc88c6ba45521cecfc2793078367ff20e
prashantp1 <prashant.patil@druva.com>
2019-09-06 16:48:47 +0530

    Adding more files 2
    Signed-off-by: prashantp1 <prashant.patil@druva.com>

---
+++ array/quick_sort.cpp
@@ -0,0 +1,80 @@
+/* C++ implementation of QuickSort using Hoare's 
+partition scheme. */
+#include<bits/stdc++.h> 
+using namespace std; 
+
+/* This function takes last element as pivot, places 
+the pivot element at its correct position in sorted 
+	array, and places all smaller (smaller than pivot) 
+to left of pivot and all greater elements to right 
+of pivot */
+int partition(int arr[], int low, int high) 
+{ 
+	int pivot = arr[low]; 
+	int i = low - 1, j = high + 1; 
+
+	while (true) 
+	{ 
+		// Find leftmost element greater than 
+		// or equal to pivot 
+		do
+		{ 
+			i++; 
+		} while (arr[i] <= pivot); 
+
+		// Find rightmost element smaller than 
+		// or equal to pivot 
+		do
+		{ 
+			j--; 
+		} while (arr[j] > pivot); 
+
+		// If two pointers met. 
+		if (i >= j) 
+		{
+			swap(arr[j], arr[low]);
+			return j; 
+		}
+
+		swap(arr[i], arr[j]); 
+	} 
+
+} 
+
+/* The main function that implements QuickSort 
+arr[] --> Array to be sorted, 
+low --> Starting index, 
+high --> Ending index */
+void quickSort(int arr[], int low, int high) 
+{ 
+	if (low < high) 
+	{ 
+		/* pi is partitioning index, arr[p] is now 
+		at right place */
+		int pi = partition(arr, low, high); 
+
+		// Separately sort elements before 
+		// partition and after partition 
+		quickSort(arr, low, pi); 
+		quickSort(arr, pi + 1, high); 
+	} 
+} 
+
+/* Function to print an array */
+void printArray(int arr[], int n) 
+{ 
+	for (int i=0; i < n; i++) 
+		printf("%d ", arr[i]); 
+	printf("\n"); 
+} 
+
+// Driver program to test above functions 
+int main() 
+{ 
+	int arr[] = {10, 7, 8, 9, 42, 44, 46, 48, 32, 36, 37, 11, 1, 5}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	quickSort(arr, 0, n-1); 
+	printf("Sorted array: \n"); 
+	printArray(arr, n); 
+	return 0; 
+} 
